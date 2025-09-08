d6bb46b804b1e92893fd15411c1da766187d1ed3
IceChes <61756119+IceChes@users.noreply.github.com>
2025-09-07 22:46:19 -0500

    Update bluepad_spinner_servo_desc.ino

--- Firmware/Production Firmware/bluepad_spinner_servo_desc/bluepad_spinner_servo_desc.ino
+++ Firmware/Production Firmware/bluepad_spinner_servo_desc/bluepad_spinner_servo_desc.ino
@@ -13,6 +13,12 @@
 #define INVERT_MOTOR_1 false
 #define INVERT_MOTOR_2 true
 
+#define VERBOSE_LOGGING false
+
+//HW address for my stadia controller: BTHLE\Dev_f28d95d501e6
+
+uint8_t controller_mac[6] = {0xF2, 0x8D, 0x95, 0xD5, 0x01, 0xE6};
+
 Servo motor_control;
 
 //timer vars
@@ -26,16 +32,15 @@ int esc_value = 1000;
 int target_esc_value;
 int motor_1_value;
 int motor_2_value;
-bool inverted = true;
+bool inverted = false;
 bool weapon_locked = false;
 bool data_updated;
 
-
 //controller vars
-int ls;   //left stick
-int rs;   //right stick
+int ls;       //left stick
+int rs;       //right stick
 int used_ls;  //the stick value the code will actually use
-int used_rs; 
+int used_rs;
 bool lb;  //left bumper
 bool rb;  //right bumper
 int lt;   //left trigger
@@ -58,11 +63,6 @@ void setup() {
   pinMode(MOTOR_2_PIN, OUTPUT);
   pinMode(WEAPON_PIN, OUTPUT);
 
-  //Bluepad controller connection info.
-  Serial.printf("BLUEPAD: Firmware: %s\n", BP32.firmwareVersion());
-  const uint8_t* addr = BP32.localBdAddress();
-  Serial.printf("BLUEPAD: BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
-
   // Setup the Bluepad callbacks
   BP32.setup(&onConnectedController, &onDisconnectedController);
 }
@@ -79,15 +79,13 @@ void loop() {
   //Motor inverts
   if (INVERT_MOTOR_1) {
     used_ls = map(ls, -512, 512, 512, -512);
-  }
-  else{
+  } else {
     used_ls = ls;
   }
 
   if (INVERT_MOTOR_2) {
     used_rs = map(rs, -512, 512, 512, -512);
-  }
-  else{
+  } else {
     used_rs = rs;
   }
 
@@ -105,6 +103,9 @@ void loop() {
     weapon_locked = !weapon_locked;
     rumble(200, 255, 0, myControllers[0]);
     last_button_press = timer;
+    if (VERBOSE_LOGGING) {
+      Serial.println("INFO: Throttle lock tripped.");
+    }
   }
 
   //If the throttle is locked, just make the target ESC value the current ESC value
@@ -121,6 +122,9 @@ void loop() {
       inverted = false;
       rumble(200, 255, 0, myControllers[0]);
       last_button_press = timer;
+      if (VERBOSE_LOGGING) {
+        Serial.println("INFO: Now inverted!");
+      }
     }
   } else if (!inverted) {
     motor_1_value = map(used_ls, -512, 512, MOTORS_MINIMUM_VALUE, MOTORS_MAXIMUM_VALUE);
@@ -129,6 +133,9 @@ void loop() {
       inverted = true;
       rumble(200, 0, 255, myControllers[0]);
       last_button_press = timer;
+      if (VERBOSE_LOGGING) {
+        Serial.println("INFO: Now reverted!");
+      }
     }
   }
 
@@ -137,9 +144,9 @@ void loop() {
   motor_control.write(WEAPON_PIN, esc_value);
 
   //If it's been 10ms since the last debug message, print a new one.
-  if (timer - last_debug_message > 10) {
+  if (VERBOSE_LOGGING && timer - last_debug_message > 10) {
     Serial.println("INFO: CONTROLLER DUMP: DP: " + String(dpad_state) + ", BT: " + String(buttons_state) + ", LS: " + String(ls) + ", RS: " + String(rs) + ", LT: " + String(lt) + ", RT: " + String(rt));
-    Serial.println("INFO: VARIABLE DUMP  : IV: " + String(inverted) + ", WP: " + String(esc_value) + ", LM: " + String(motor_1_value) + ", RM: " + String(motor_2_value));
+    Serial.println("INFO: VARIABLE DUMP  : IV: " + String(inverted) + ", WP: " + String(map(esc_value, 1000, 2000, 0, 100)) + ", LM: " + String(map(motor_1_value, 1000, 2000, -100, 100)) + ", RM: " + String(map(motor_2_value, 1000, 2000, -100, 100)));
   }
   /*Reminder to myself of what variables we have
   ls = ctl->axisY();
@@ -183,39 +190,36 @@ void triggerFailsafe() {
 
 //Bluepad32 connection callback. I don't know how this works, but I am going to add some failsafe code in there.
 void onConnectedController(ControllerPtr ctl) {
-  bool foundEmptySlot = false;
-  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
-    ControllerProperties properties = ctl->getProperties();
-    if (myControllers[i] == nullptr) {
-      Serial.println("BLUEPAD: Controller is connected!");
-      myControllers[i] = ctl;
-      foundEmptySlot = true;
-      ctl->playDualRumble(0 /* delayedStartMs */, 500 /* durationMs */, 127 /* weakMagnitude */, 127 /* strongMagnitude */);
-      break;
+  Serial.println("BLUEPAD: Controller is connected!");
+  myControllers[0] = ctl;
+  ControllerProperties properties = ctl->getProperties();
+  bool valid_controller;
+
+  for (int m = 0; m < 6; m++) {
+    if (controller_mac[m] != properties.btaddr[m]) {
+      valid_controller = false;
+    } else {
+      valid_controller = true;
     }
   }
-  if (!foundEmptySlot) {
-    Serial.println("BLUEPAD: Controller connected, but could not found empty slot");
+  if (valid_controller) {
+    Serial.println("INFO: Controller validated and bound.");
+    rumble(200, 255, 100, myControllers[0]);
+  } else {
+    ctl->disconnect();
+    BP32.enableNewBluetoothConnections(true);
+    Serial.println("ERROR: MAC section invalid. Rejecting pairing attempt.");
   }
 }
+
+//ctl->playDualRumble(0 /* delayedStartMs */, 500 /* durationMs */, 127 /* weakMagnitude */, 127 /* strongMagnitude */);
 
 //Bluepad32 disconnect callback
 void onDisconnectedController(ControllerPtr ctl) {
   bool foundController = false;
-
-  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
-    if (myControllers[i] == ctl) {
-      Serial.println("BLUEPAD: Lost controller connection");
-      triggerFailsafe();
-      myControllers[i] = nullptr;
-      foundController = true;
-      break;
-    }
-  }
-
-  if (!foundController) {
-    Serial.println("BLUEPAD: Controller disconnected, but not found in myControllers");
-  }
+  Serial.println("ERROR: Lost controller connection! Failsafing...");
+  BP32.enableNewBluetoothConnections(true);
+  triggerFailsafe();
 }
 
 void processGamepad(ControllerPtr ctl) {

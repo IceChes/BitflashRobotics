f9daaa312b83a6333dc110e0f5b3c986ad045d29
IceChes <61756119+IceChes@users.noreply.github.com>
2025-09-19 15:14:41 -0500

    More updates

--- Firmware/Test Firmware/sdmd_serial_check/sdmd_serial_check.ino
+++ Firmware/Test Firmware/sdmd_serial_check/sdmd_serial_check.ino
@@ -1,11 +1,13 @@
-
-
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(4, 3); // 4=RX, 3=TX (we won't TX)
 void setup() {
   Serial.begin(9600);
+  mySerial.begin(9600);
 }
 void loop() {
-  if (Serial.available()) {
-    digitalWrite(5, 1);
-    digitalWrite(6, 0);
+  if (mySerial.available()) {
+    int r = mySerial.read();
+    Serial.print("0x");
+    Serial.println(r, HEX); // show hex so you can spot patterns
   }
 }
